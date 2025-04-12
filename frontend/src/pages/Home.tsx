7fd2f8322e7e0618252f0076e49e6f8b0de7a291
thashin04 <thashin2004@gmail.com>
2025-04-12 16:29:34 -0400

    home page type shi

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,48 +1,91 @@
-import React, { MouseEvent, useEffect, useState } from "react"
-import supabase from "@/supabase-client";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "@/middleware";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import hero from "../assets/hero.svg";
+import bulb from "../assets/lightbulb.png";
+import sparkle from "../assets/titlesparkle.svg";
+import FeatureCarousel from "../components/ui/FeatureCarousel"
 
 const Home: React.FC = () => {
-  const navigate = useNavigate();
-
-  const [name, setName] = useState('')
-
-  const { session } = useAuth();
+  const location = useLocation();
 
   useEffect(() => {
-    if (session?.user?.id) {
-      setName(session.user.user_metadata.full_name);
+    const hash = location.hash;
+    if (hash) {
+      const el = document.querySelector(hash);
+      if (el) {
+        setTimeout(() => {
+          el.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
     }
-  }, [session]);
-
-  const logout = async (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-
-    const { error } = await supabase.auth.signOut();
-    if(error) {
-      console.error(error)
-      alert(error)
-      return
-    }
-
-    navigate("/login")
-  }
-
+  }, [location]);
   return (
     <>
-      <div>
-        Home | Hey {name}
-      </div>
-      <div>
-        <button
-          type="button"
-          onClick={logout}
-          className="text-blue-500 hover:underline"
-        >
-          Logout
-        </button>
-      </div>
+      <section className="flex flex-col-reverse lg:flex-row items-center max-w-[1440px] mx-auto justify-between px-6 mt-10 lg:px-20 py-14 shadow-xl bg-offwhite" id="home">
+          <div className="lg:w-1/2 text-center lg:text-left space-y-6">
+            <h1 className="text-3xl tracking-wide lg:text-5xl font-bold">
+              A dyslexia converter for your everyday basic needs
+            </h1>
+            <p className="text-xl tracking-wider ">
+              Easily customize your files, texts, and images into your own customized format.
+            </p>
+            <button className="px-6 py-2 bg-darkgray hover:bg-darkgray/80 hover:shadow-md/30 hover:-translate-y-0.5 rounded-full border-[1.5px] border-black text-black tracking-wider transition-all cursor-pointer">
+              Try Now
+            </button>
+          </div>
+          <div className="lg:w-1/2 m-10 lg:mt-0 2xl:w-1/4">
+            <img
+              src={hero}
+              alt="hero"
+              className="w-full h-auto transition-all duration-300 ease-in-out"
+            />
+          </div>
+      </section>
+
+      <section className="flex flex-col-reverse lg:flex-row items-center max-w-[1440px] mx-auto justify-between px-6 lg:px-20 py-22" id="about">
+        <div className="lg:w-1/2 text-center lg:text-left space-y-6">
+          <h1 className="text-3xl lg:text-5xl tracking-wide font-bold">
+          Clarify simplifies everyday digital tasks
+          </h1>
+          <p className="text-xl  tracking-wider">
+          Whether you’re converting documents, text, or images, we make it seamless and fast so you can focus on what matters most.
+          </p>
+        </div>
+        <div className="lg:w-1/4 w-1/3 m-10 py-12 lg:mt-0">
+          <img
+            src={bulb}
+            alt="hero"
+            className="w-full h-auto transition-all duration-300 ease-in-out"
+          />
+        </div>
+      </section>
+      
+      <section className="px-6 lg:px-20 pb-12 max-w-[1440px] mx-auto" id="features">
+        <div className="flex flex-row pb-12">
+        <img
+              src={sparkle}
+              alt="sparkle-bullet-point"
+              className="w-14 h-auto transition-all duration-300 ease-in-out"
+          />
+        <h2 className="text-3xl tracking-wider lg:text-4xl font-bold text-center sparkle-text pl-4">Key Features</h2>
+        </div>
+        <FeatureCarousel />
+        
+      </section>
+
+      <section className="px-6 lg:px-20 py-16 max-w-[1440px] mx-auto" id="demo">
+        <div className="flex flex-row pb-12">
+        <img
+              src={sparkle}
+              alt="sparkle-bullet-point"
+              className="w-14 h-auto transition-all duration-300 ease-in-out"
+          />
+        <h2 className="text-3xl tracking-wider lg:text-4xl font-bold text-center max-sm:text-left sparkle-text pl-4">How to use Clarify</h2>
+        
+        </div>
+        <iframe className="w-full h-96" src="https://www.youtube.com/embed/YSWMYnuOImg?si=FzVo7W5Z32MRkHwO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+      </section>
     </>
   );
 };
